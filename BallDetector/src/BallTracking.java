2019f773b39085b6959f7e172e60d5dfa4c49ccc
fallXone <conner4892@gmail.com>
2014-03-27 19:28:43 -0400

    Added circle detection

--- BallDetector/src/BallTracking.java
+++ BallDetector/src/BallTracking.java
@@ -7,13 +7,26 @@ import javax.swing.JPanel;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
 
-
-public class ObjectDetectionFrame {
+/************************************************************
+ * Conner Fallone & Matt Shrider
+ * CS-365
+ * Winter 2014
+ * OpenCV circle detection and tracking in a video
+ ************************************************************/
+public class BallTracking {
 	
 	public static void main(String[] args){
+		
+		// Variables
 		String videoPath = "C:/Users/Conner Fallone/Documents/GitHub/ImageRecognition/BallDetector/ball.avi";
+		Scalar redColor = new Scalar(255, 0, 0);
+		Size size = new Size(9,9);
 		
 		// Load library
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -21,17 +34,22 @@ public class ObjectDetectionFrame {
 		// Swing Frame
 		JFrame frame = new JFrame("Object Detection");
 		
-		// Frame Properties
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		frame.setSize(400,400);
 		
 		// Swing Panel
 		CVPanel panel = new CVPanel();
+		
+		// Frame Properties
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		frame.setSize(400,400);
 		frame.setContentPane(panel);
 		frame.setVisible(true);
 		
-		// Initialize Video
+		// Images to use
 		Mat videoImage = new Mat();
+		Mat grey = new Mat();
+		Mat circles = new Mat();
+		
+		// Stream from .avi
 		// VideoCapture stream = new VideoCapture(videoPath);
 		
 		// Stream with webcam
@@ -39,12 +57,36 @@ public class ObjectDetectionFrame {
 		
 		if (stream.isOpened()){
 			while (true){
-				stream.read(videoImage);
-				
-				if( !videoImage.empty() )  
-		         {  
-		           frame.setSize(videoImage.width()+40,videoImage.height()+60);
-		           panel.matToBufferedImage(videoImage);
+				stream.read(videoImage);		
+				if(!videoImage.empty()){
+		           frame.setSize(grey.width()+40,grey.height()+60);
+		           
+		           // Convert to grayscale
+		           Imgproc.cvtColor(videoImage, grey, Imgproc.COLOR_RGB2GRAY);
+		           
+		           // Apply a Gaussian blur to reduce noise and avoid false circle detection:
+		           Imgproc.GaussianBlur(grey, grey, size, 2, 2 );
+		           
+		           // Apply Hough Circle Transform:
+		           Imgproc.HoughCircles(grey, circles, Imgproc.CV_HOUGH_GRADIENT, 1, grey.rows()/8, 200, 100, 0, 0 );
+		           
+		           // Draw Circles
+		           for (int i = 0; i < Math.min(circles.cols(), 10); i++){
+		        	   double vCircle[] = circles.get(0,  i);
+		        	   
+		        	   if (vCircle == null)
+		        		   break;
+		        	   
+		        	   Point center = new Point(Math.round(vCircle[0]), Math.round(vCircle[1]));
+		        	   int radius = (int)Math.round(vCircle[2]);
+		        	   
+		        	   // Draw the found circle
+		        	   Core.circle(grey, center, radius, redColor, 2);
+		           }
+		           
+		           // Paint the image to the panel
+		           panel.matToBufferedImage(grey);
+		           //panel.matToBufferedImage(videoImage);
 		           panel.repaint();
 		         }  
 			}
@@ -52,7 +94,7 @@ public class ObjectDetectionFrame {
 	}
 }
 
-//converts each frame to buffered images for analysis
+// Converts each frame to buffered images for analysis
 // Source: http://cell0907.blogspot.com/2013/06/creating-windows-and-capturing-webcam.html
 class CVPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
